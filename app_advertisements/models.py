a3690e8d2e79f6522cf5cd674f517b341ad63fdd
Timofey <tima5758@mail.ru>
2023-08-14 18:18:53 +0300

    Выполнено домашнее задание к уроку 8

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 class Advertisement(models.Model):
     title = models.CharField('заголовок', max_length=128)
@@ -13,4 +15,24 @@ class Advertisement(models.Model):
 
     class Meta:
         db_table = 'advertisements'
+
+    @admin.display(description='дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style = "color: green; font-weight: bold;"> Сегодня в {} </span>', created_time
+            )
+        return self.created_at.strftime('%d.%m.%y в %H:%M:%S')
+
+    @admin.display(description='дата последнего обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style = "color: blue; font-weight: bold;"> Сегодня в {} </span>', updated_time
+            )
+        return self.updated_at.strftime('%d.%m.%y в %H:%M:%S')
 # Create your models here.
