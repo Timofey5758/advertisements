9a4451f686f0184bd4285d92487c0ee627e82ad8
Timofey <tima5758@mail.ru>
2023-08-22 20:06:13 +0300

    Выполнено домашнее задание к уроку 9

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Advertisement
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'auction', 'created_date', 'updated_date']
+    list_display = ['id', 'title', 'description', 'price', 'auction', 'created_date', 'updated_date', 'get_image']
     list_filter = ['auction', 'created_at']
 
     actions = ['make_auction_as_false', 'make_auction_as_true']
@@ -17,7 +17,7 @@ class AdvertisementAdmin(admin.ModelAdmin):
 
     fieldsets = (
         ('Общие', {
-            'fields': ('title', 'description')
+            'fields': ('title', 'description', 'image')
         }),
         ('Финансы', {
             'fields': ('price', 'auction'),
--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 class Advertisement(models.Model):
     title = models.CharField('заголовок', max_length=128)
     description = models.TextField('описание')
@@ -9,7 +11,8 @@ class Advertisement(models.Model):
     auction = models.BooleanField('торг', help_text='Отметьте, если торг уместен')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('изображение', upload_to='advertisements/')
     def __str__(self):
         return f'Advertisement(id = {self.id}, title = {self.title}, price = {self.price})'
 
@@ -35,4 +38,11 @@ class Advertisement(models.Model):
                 '<span style = "color: blue; font-weight: bold;"> Сегодня в {} </span>', updated_time
             )
         return self.updated_at.strftime('%d.%m.%y в %H:%M:%S')
+
+    @admin.display(description='фото')
+    def get_image(self):
+        if self.image:
+            return format_html(
+                '<img src="{url}" style="max-width: 80px; max-height: 80px;">', url=self.image.url
+            )
 # Create your models here.
